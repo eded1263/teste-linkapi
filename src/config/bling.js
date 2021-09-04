5d157d4d491311f23190ad03b446021d09610426
Edilson <edilson@quicktech.no>
2021-09-04 10:34:09 -0300

    feature: added save order method on bling helper

--- src/config/bling.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-class BlingConnection {
-  baseUrl = "https://bling.com.br/Api/v2/";
-  apiKey = process.env.BLING_KEY;
-}
-
-export const blingConnection = new BlingConnection();
